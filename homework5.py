a7329a0b5908ffdf8a3d91bc8cb838aab6c52864
KirillR1983 <162807374+KirillR1983@users.noreply.github.com>
2024-03-15 11:55:35 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:6])
+my_list[3] = 'coconut'
+print(my_list)
+
+my_dict = {'apple': 'яблко', 'banana': 'банан', 'orange': 'апельсин', 'kiwi': 'киви'}
+print(my_dict)
+my_dict.update({'coconut': 'кокос'})
+print(my_dict)

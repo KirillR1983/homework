036ba3ae1827ad635484d803a6b80aa7f6e8deb0
KirillR1983 <162807374+KirillR1983@users.noreply.github.com>
2024-03-16 11:19:49 +0300

    Update homework5.py
    Были проведены работы  над ошибкой, добавлены 3 элемента в список и выведены на экран подсписок my_list с 3 до 5 элементов.

--- homework5.py
+++ homework5.py
@@ -1,8 +1,8 @@
-my_list = ['apple', 'banana', 'orange', 'kiwi']
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'peach', 'cherry', 'watermelon']
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
-print(my_list[3:6])
+print(my_list[3:5])
 my_list[3] = 'coconut'
 print(my_list)
 

70fcf52ef463169e2cf739f83ce0e1c88d45c604
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-03-16 11:57:04 +0300

    Update homework5.py
    Изменил индекс с [3:5], на [2:5]

--- homework5.py
+++ homework5.py
@@ -2,7 +2,7 @@ my_list = ['apple', 'banana', 'orange', 'kiwi', 'peach', 'cherry', 'watermelon']
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
-print(my_list[3:5])
+print(my_list[2:5])
 my_list[3] = 'coconut'
 print(my_list)
 

11e4a7c972946e50edc15240fe723d7cdaf57859
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-03-20 14:39:03 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,8 @@
+my_car_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+today_i_drive_ = "Я езжу на автомобиле марки:"
+for i in range(len(my_car_)):
+       print(today_i_drive_, my_car_[i])
+cars_count = int(0)
+cars_count += int(10)
+for i in range(len(my_car_)):
+       print(today_i_drive_, my_car_[i] * cars_count)

dd4ab67cb90ca4a794f3d4b9871ecaed7d677fe1
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-03-21 12:20:12 +0300

    for.py

--- for.py
+++ for.py
@@ -1,8 +1,6 @@
 my_car_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-today_i_drive_ = "Я езжу на автомобиле марки:"
-for i in range(len(my_car_)):
-       print(today_i_drive_, my_car_[i])
 cars_count = 0
 for i in range(len(my_car_)):
+       print("Я езжу на автомобиле марки:", my_car_[i])
        cars_count += 10
        print(cars_count)

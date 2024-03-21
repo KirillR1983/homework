35f3239ba90c0e0e6f4e4c2381a68a683b055289
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-03-21 19:46:34 +0300

    Add files via upload

---
+++ def_params.py
@@ -0,0 +1,5 @@
+def print_params(name): # print_params - функция, (name) - аргумент
+    print('Hello', name) # возвращаем функцию print
+print_params('Kirill') # вызов функции в которой передаём значение аргумента (name)
+print_params('Evgeniy')
+

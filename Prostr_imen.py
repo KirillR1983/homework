1edd8608f7554775b3c1c2f38db133766ae91316
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-03-30 10:19:55 +0300

    Add files via upload

---
+++ Prostr_imen.py
@@ -0,0 +1,15 @@
+def test():
+    a = 1
+    b = 10
+    print(a, b)
+test()
+
+
+print('------------------------------------')
+
+def test2():
+    a = 20
+    b = 30
+    c = 40
+    print(a, b, c)
+test2()

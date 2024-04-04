c69a1d582e5e3dc62dd1dabd57d3bef66a9d1c0f
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-04-04 16:09:47 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,12 @@
+# Код 2х функций импортируемый из файла mod_pack.py
+
+# def print_hi():
+#     print('Hello,')
+#
+# def print_name():
+#     print('World!')
+
+from mod_pack import *
+
+print_hi()
+print_name()

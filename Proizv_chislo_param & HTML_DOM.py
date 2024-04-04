04d8eda64da2a5c972fcf88dd564b5866b5cb0e0
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-04-04 12:21:04 +0300

    Proizv_chislo_param & HTML_DOM.py

--- Proizv_chislo_param & HTML_DOM.py
+++ Proizv_chislo_param & HTML_DOM.py
@@ -6,7 +6,7 @@ test_('a', 25, 58.365, 'MB', True)
 print('-------------------------------------')
 
 def summa(n):
-    if n == 1:
+    if n == 0:
         return 1
     else:
         return n * summa(n - 1)

ee28bf8a4dc4c084acbb7ca20e5a7aca2ac36493
Kirill <162807374+KirillR1983@users.noreply.github.com>
2024-04-04 11:05:23 +0300

    Add files via upload

---
+++ Proizv_chislo_param & HTML_DOM.py
@@ -0,0 +1,14 @@
+def test_(*args):
+    print(*args)
+
+test_('a', 25, 58.365, 'MB', True)
+
+print('-------------------------------------')
+
+def summa(n):
+    if n == 1:
+        return 1
+    else:
+        return n * summa(n - 1)
+
+print(summa(7))
